0a2d0227aa9bfea9bc33fb2b5122a64d966ec53f
Therbian-Granbat <142832193+Therbian-Granbat@users.noreply.github.com>
2023-09-05 10:04:01 +0000

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,5 @@
+function calculateNumbers(var1, var2)           // Function
+{
+    var total = var1 + var2;                    // Variable
+    return total;                               // Return value
+}
